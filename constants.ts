becb5f5ad9c3becc1771ce0df1976079cc9fe804
dakshbhardwajjee2026-netizen <dakshbhardwajjee2026@gmail.com>
2026-09-15 23:11:12 +0530

    feat: proactive memory recalling, voice note playback, and CST reminiscence in constants.ts

--- constants.ts
+++ constants.ts
@@ -85,6 +85,7 @@ export const DEFAULT_VOICE_NOTES: VoiceNote[] = [
   {
     id: 'vn-1',
     audioData: '',
+    message: "Hi Dad, I love you so much! I'm bringing your favorite cookies and fresh tea this afternoon at 2 PM. See you very soon!",
     timestamp: Date.now() - 1000 * 60 * 45, // 45 mins ago
     played: false,
     senderName: 'Sarah (Daughter)',
